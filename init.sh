993157920fde31782942c4ce8e561d4dff16c09c
xlgwr <xlgwr@163.com>
2013-11-15 16:44:29 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,11 +3,13 @@
    apt-get install zsh
    add-apt-repository ppa:apt-fast/stable
    add-apt-repository ppa:cassou/emacs
+   add-apt-repository ppa:chris-lea/node.js-legacy
    apt-get install apt-fast
    apt-get install emacs24
+   apt-get install nodejs
    apt-get install curl
    curl -L get.rvm.io | bash -s stable
    rvm install 2.0.0
    gem install rails --no-doc --no-ri
    gem update --no-doc --no-ri
-   apt-get install nodejs
+   
