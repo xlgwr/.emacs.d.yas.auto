763fdc241bd240321a358e2da907a8209027d1e6
xlgwr <xlgwr@163.com>
2014-04-06 00:55:25 +0800

    update

--- init.sh
+++ init.sh
@@ -1,16 +1,16 @@
-   apt-get update
-   apt-get upgrade
    apt-get install zsh
    apt-get install fbterm
    add-apt-repository ppa:apt-fast/stable
    add-apt-repository ppa:cassou/emacs
    add-apt-repository ppa:chris-lea/node.js-legacy
+   apt-get update
+   apt-get upgrade
    apt-get install apt-fast
-   apt-get install emacs24
-   apt-get install nodejs
-   apt-get install curl
+   apt-fast install emacs24
+   apt-fast install nodejs
+   apt-fast install curl
    curl -L get.rvm.io | bash -s stable
-   rvm install 2.0.0
+   rvm install 2.1.0
    gem install rails --no-doc --no-ri
    gem update --no-doc --no-ri
    
