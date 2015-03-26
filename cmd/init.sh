8ca6d0538cf9c19a44314e057d4727e4c8a05073
xlgwr <xlgwr@163.com>
2015-03-26 21:38:00 +0800

    edit cmd

--- cmd/init.sh
+++ cmd/init.sh
@@ -1,18 +1,11 @@
-sudo apt-get install zsh fbterm
-sudo add-apt-repository ppa:apt-fast/stable
-sudo add-apt-repository ppa:cassou/emacs
-sudo add-apt-repository ppa:chris-lea/node.js-legacy
-sudo add-apt-repository ppa:rwky/redis
-sudo apt-get update && sudo apt-get install apt-fast && sudo apt-get install libc6:i386 libstdc++6:i386 emacs24 node curl libxss1 && sudo apt-get install redis mongodb-org && sudo apt-get upgrade
-curl -L get.rvm.io | bash -s stable
-. ~/.rvm/scripts/rvm
-rvm install ruby
-gem sources --remove https://rubygems.org/
-gem sources -a https://ruby.taobao.org/
-gem install rails --no-doc --no-ri
-gem update --no-doc --no-ri
+#14.04 later
+sudo add-apt-repository ppa:saiarcot895/myppa
+sudo apt-get update
+sudo apt-get -y install apt-fast && sudo apt-fast install zsh fbterm emacs24 libxss1 && sudo apt-fast upgrade && sudo apt-fast dist-upgrade
+sudo apt-get install curl wget && curl -sL https://deb.nodesource.com/setup_0.12 | sudo bash - && sudo npm install -g cnpm --registry=https://registry.npm.taobao.org
+#libc6:i386 libstdc++6:i386
+curl -L get.rvm.io | bash -s stable && . ~/.rvm/scripts/rvm && rvm install ruby && gem sources --remove https://rubygems.org/ && gem sources -a https://ruby.taobao.org/ && gem install rails --no-doc --no-ri && gem update --no-doc --no-ri
 # ntlmaps-setup-0.9.9.6 proxy
-sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb  
-sudo dpkg -i google-chrome*.deb  
+sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome*.deb  
    
 
